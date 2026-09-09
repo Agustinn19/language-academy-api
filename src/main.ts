7b6f6767e60e586fc7e91b8192152f1bfa4a9fd0
Agustinn19 <agustin19qy@gmail.com>
2026-09-09 18:19:15 -0300

    fix(security): configure CORS explicitly using CORS_ORIGIN env variable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.use(helmet());
-  
-  app.enableCors();
+
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN'),
+  });
 
   app.setGlobalPrefix('api');
 
